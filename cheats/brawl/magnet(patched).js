6eb1f57d93e3c26b1892407da7a1eb03a0cb8dcd
redhorse26 <redhorse26@users.noreply.github.com>
2025-11-16 19:15:53 +0800

    Update magnet(patched).js

--- cheats/brawl/magnet(patched).js
+++ cheats/brawl/magnet(patched).js
@@ -1,69 +1,63 @@
 /**
  * @license AGPL-3.0
- * Blooket Cheats
- * Copyright (C) 2023-present 05Konz
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published
- * by the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- * 
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
- * Source: https://github.com/Blooket-Council/Blooket-Cheats 05konz994@gmail.com
-*/
-
-/* THE UPDATE CHECKER IS ADDED DURING COMMIT PREP, THERE MAY BE REDUNDANT CODE, DO NOT TOUCH */
+ * Blooket Brawl - Permanent Magnet
+ * Gives permanent magnet effect
+ */
 
 (() => {
-    let iframe = document.querySelector("iframe");
-    if (!iframe) {
-        iframe = document.createElement("iframe");
-        iframe.style.display = "none";
-        document.body.append(iframe);
+    console.log("🧲 Permanent Magnet\n");
+    
+    if (!window.Phaser) {
+        alert("❌ Phaser not found!");
+        return;
     }
-    /* By CryptoDude3 */
-    if (window.fetch.call.toString() == 'function call() { [native code] }') {
-        const call = window.fetch.call;
-        window.fetch.call = function () {
-            if (!arguments[1].includes("s.blooket.com/rc")) return call.apply(this, arguments);
+    
+    console.log("✓ Phaser", window.Phaser.VERSION);
+    
+    // Hook groups
+    if (window.Phaser.GameObjects?.Group?.prototype && !window._groupHooked) {
+        const originalUpdate = window.Phaser.GameObjects.Group.prototype.preUpdate || 
+                              window.Phaser.GameObjects.Group.prototype.update;
+        
+        if (originalUpdate) {
+            window.Phaser.GameObjects.Group.prototype.preUpdate = function(...args) {
+                if (!window._groups) window._groups = new Set();
+                window._groups.add(this);
+                return originalUpdate?.apply(this, args);
+            };
+            console.log("✓ Group hook installed");
+            window._groupHooked = true;
         }
     }
-    const timeProcessed = 1730769902683;
-    let latestProcess = -1;
-    const cheat = (async () => {
-        Object.values((function react(r = document.querySelector("body>div")) { return Object.values(r)[1]?.children?.[0]?._owner.stateNode ? r : react(r.querySelector(":scope>div")) })())[1].children[0]._owner.stateNode.game.current.config.sceneConfig.physics.world.colliders._active.find(x => x.collideCallback?.toString().includes('magnetTime')).collideCallback({ active: true }, { active: true, setActive() { }, setVisible() { } });
-    });
-    let img = new Image;
-    img.src = "https://raw.githubusercontent.com/Blooket-Council/Blooket-Cheats/main/autoupdate/timestamps/brawl/magnet.png?" + Date.now();
-    img.crossOrigin = "Anonymous";
-    img.onload = function() {
-        const c = document.createElement("canvas");
-        const ctx = c.getContext("2d");
-        ctx.drawImage(img, 0, 0, this.width, this.height);
-        let { data } = ctx.getImageData(0, 0, this.width, this.height), decode = "", last;
-        let i = 0;
-        while (i < data.length) {
-            let char = String.fromCharCode(data[i % 4 == 3 ? (i++, i++) : i++] + data[i % 4 == 3 ? (i++, i++) : i++] * 256);
-            decode += char;
-            if (char == "/" && last == "*") break;
-            last = char;
+    
+    console.log("\n⏳ Waiting for scene...");
+    
+    setTimeout(() => {
+        if (!window._groups || window._groups.size === 0) {
+            alert("❌ Scene not found! Make sure you're playing!");
+            return;
         }
-        let _, time = timeProcessed, error = "There was an error checking for script updates. Run cheat anyway?";
-        try {
-            [_, time, error] = decode.match(/LastUpdated: (.+?); ErrorMessage: "((.|\n)+?)"/);
-        } catch (e) {}
-        if ((latestProcess = parseInt(time)) <= timeProcessed || iframe.contentWindow.confirm(error)) cheat();
-    }
-    img.onerror = img.onabort = () => {
-        img.onerror = img.onabort = null;
-        cheat();
-        let iframe = document.querySelector("iframe");
-        iframe.contentWindow.alert("It seems the GitHub is either blocked or down.\n\nIf it's NOT blocked, join the Discord server for updates\nhttps://discord.gg/jHjGrrdXP6\n(The cheat will still run after this alert)")
-    }
+        
+        const scene = Array.from(window._groups)[0].scene;
+        
+        if (!scene || !scene.playerService || !scene.playerService.player) {
+            alert("❌ Player not found!");
+            return;
+        }
+        
+        const player = scene.playerService.player;
+        
+        console.log("\n🧲 Current magnetTime:", player.magnetTime);
+        
+        // Set magnet time to 999999 seconds (basically permanent)
+        player.magnetTime = 999999;
+        
+        console.log("✅ New magnetTime:", player.magnetTime);
+        console.log("\n🧲 Permanent magnet activated!");
+        console.log("All drops will now be attracted to you!");
+        
+        alert("✅ Permanent Magnet!\n\n🧲 You now have a magnet effect for 999999 seconds!\n\nAll XP and items will be pulled to you automatically!");
+        
+    }, 1000);
+    
 })();
