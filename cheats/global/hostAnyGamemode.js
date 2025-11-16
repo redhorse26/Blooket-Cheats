192916ae4de659b45c5f713fb26ea31fbe89691f
redhorse26 <redhorse26@users.noreply.github.com>
2025-11-16 15:58:53 +0800

    Update hostAnyGamemode.js
    test

--- cheats/global/hostAnyGamemode.js
+++ cheats/global/hostAnyGamemode.js
@@ -45,7 +45,7 @@
         if (location.pathname == "/host/settings") {
             const gamemodes = ["Racing", "Classic", "Factory", "Cafe", "Defense2", "Defense", "Royale", "Gold", "Candy", "Brawl", "Hack", "Pirate", "Fish", "Dino", "Toy", "Rush"],
                 type = prompt(`Which gamemode do you want to switch to? (Case sensitive)\n${gamemodes.slice(0, gamemodes.length - 1).join(", ")} or ${gamemodes[gamemodes.length - 1]}`);
-            if (gamemodes.includes(type))
+            if (true)
                 Object.values((function react(r = document.querySelector("body>div")) { return Object.values(r)[1]?.children?.[0]?._owner.stateNode ? r : react(r.querySelector(":scope>div")) })())[1].children[0]._owner.stateNode.setState({ settings: { type } });
             else alert("Gamemode not found, make sure you spelled and capitalized it right.");
         } else alert("Run this script on the host settings page");
