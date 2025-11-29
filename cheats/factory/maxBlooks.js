440854e4c9e7a1b000a61018664790b1cf1d2058
redhorse26 <alexsun@duck.com>
2025-11-29 23:27:56 +0800

    Update maxBlooks.js

--- cheats/factory/maxBlooks.js
+++ cheats/factory/maxBlooks.js
@@ -1,69 +1,61 @@
 /**
  * @license AGPL-3.0
- * Blooket Cheats
- * Copyright (C) 2023-present 05Konz
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published
- * by the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- * 
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
- * Source: https://github.com/Blooket-Council/Blooket-Cheats 05konz994@gmail.com
-*/
-
-/* THE UPDATE CHECKER IS ADDED DURING COMMIT PREP, THERE MAY BE REDUNDANT CODE, DO NOT TOUCH */
+ * Blooket Factory - Max Blooks (Level 4)
+ * Sets all active blooks to max level immediately
+ */
 
 (() => {
-    let iframe = document.querySelector("iframe");
-    if (!iframe) {
-        iframe = document.createElement("iframe");
-        iframe.style.display = "none";
+    console.clear();
+    console.log("💉 Factory: Maxing Levels...");
+
+    let foundFactories = null;
+
+    // Deep Scanner
+    function scanDeep(obj, depth = 0) {
+        if (depth > 8 || foundFactories) return;
+        if (!obj || typeof obj !== 'object') return;
+
+        if (obj.factories && Array.isArray(obj.factories) && obj.factories.length > 0) {
+            const sample = obj.factories[0];
+            if (sample.cash && sample.level !== undefined) {
+                foundFactories = obj.factories;
+                return;
+            }
+        }
+
+        if (Array.isArray(obj)) {
+            obj.forEach(item => scanDeep(item, depth + 1));
+        } else {
+            if (obj.props) scanDeep(obj.props, depth + 1);
+            if (obj.children) scanDeep(obj.children, depth + 1);
+            if (obj.memoizedProps) scanDeep(obj.memoizedProps, depth + 1);
+            if (obj.memoizedState) scanDeep(obj.memoizedState, depth + 1);
+        }
+    }
+
+    const root = document.querySelector('#app') || document.body;
+    function traverseDOM(node) {
+        if (foundFactories) return;
+        const k = Object.keys(node).find(key => key.startsWith('__reactFiber'));
+        if (k) scanDeep(node[k]);
+        for (const child of node.children) traverseDOM(child);
+    }
+    traverseDOM(root);
+
+    if (foundFactories) {
+        let count = 0;
+        foundFactories.forEach(factory => {
+            factory.level = 4;
+            // Optional: Reset time/cash to match level 4 if needed, 
+            // but usually just setting level unlocks the sprite
+            count++;
+        });
+
+        let iframe = document.createElement('iframe');
         document.body.append(iframe);
-    }
-    /* By CryptoDude3 */
-    if (window.fetch.call.toString() == 'function call() { [native code] }') {
-        const call = window.fetch.call;
-        window.fetch.call = function () {
-            if (!arguments[1].includes("s.blooket.com/rc")) return call.apply(this, arguments);
-        }
-    }
-    const timeProcessed = 1730769905951;
-    let latestProcess = -1;
-    const cheat = (async () => {
-        Object.values((function react(r = document.querySelector("body>div")) { return Object.values(r)[1]?.children?.[0]?._owner.stateNode ? r : react(r.querySelector(":scope>div")) })())[1].children[0]._owner.stateNode.state.blooks.forEach(blook => blook.level = 4);
-    });
-    let img = new Image;
-    img.src = "https://raw.githubusercontent.com/Blooket-Council/Blooket-Cheats/main/autoupdate/timestamps/factory/maxBlooks.png?" + Date.now();
-    img.crossOrigin = "Anonymous";
-    img.onload = function() {
-        const c = document.createElement("canvas");
-        const ctx = c.getContext("2d");
-        ctx.drawImage(img, 0, 0, this.width, this.height);
-        let { data } = ctx.getImageData(0, 0, this.width, this.height), decode = "", last;
-        let i = 0;
-        while (i < data.length) {
-            let char = String.fromCharCode(data[i % 4 == 3 ? (i++, i++) : i++] + data[i % 4 == 3 ? (i++, i++) : i++] * 256);
-            decode += char;
-            if (char == "/" && last == "*") break;
-            last = char;
-        }
-        let _, time = timeProcessed, error = "There was an error checking for script updates. Run cheat anyway?";
-        try {
-            [_, time, error] = decode.match(/LastUpdated: (.+?); ErrorMessage: "((.|\n)+?)"/);
-        } catch (e) {}
-        if ((latestProcess = parseInt(time)) <= timeProcessed || iframe.contentWindow.confirm(error)) cheat();
-    }
-    img.onerror = img.onabort = () => {
-        img.onerror = img.onabort = null;
-        cheat();
-        let iframe = document.querySelector("iframe");
-        iframe.contentWindow.alert("It seems the GitHub is either blocked or down.\n\nIf it's NOT blocked, join the Discord server for updates\nhttps://discord.gg/jHjGrrdXP6\n(The cheat will still run after this alert)")
+        iframe.contentWindow.alert(`✅ Set ${count} blooks to Level 4!\n\nIMPORTANT: Perform any action (buy/upgrade) to see the visual change.`);
+        iframe.remove();
+    } else {
+        alert("❌ Could not find factories. Make sure you have at least one blook!");
     }
 })();
