083741f02126276bced71181662e4802514d4e58
redhorse26 <redhorse26@users.noreply.github.com>
2025-11-30 18:31:05 +0800

    Update licenses and minify (excluding specific game folders)

--- cheats/global/readBeforeUsing.js
+++ cheats/global/readBeforeUsing.js
@@ -1,7 +1,9 @@
 /**
  * @license AGPL-3.0
- * Blooket Cheats
+ * Blooket Cheats (Modified Version)
+ * Copyright (C) 2023-present 05Konz
  * Copyright (C) 2025-present redhorse26
+ * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
@@ -15,6 +17,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * 
- * Source: [https://github.com/redhorse26/Blooket-Cheats/tree/main/cheats](https://github.com/redhorse26/Blooket-Cheats/tree/main/cheats)
+ * Source of original work: [https://github.com/Blooket-Council/Blooket-Cheats/tree/main](https://github.com/Blooket-Council/Blooket-Cheats/tree/main)
+ * Source of this modified work: [https://github.com/redhorse26/Blooket-Cheats/tree/main/cheats](https://github.com/redhorse26/Blooket-Cheats/tree/main/cheats)
 */
 alert("Note: Many modes updated to a new answer system (all except Fishing Frenzy and Crypto Hack in hosted modes). Therefore, Use Any Blook, Every Answer Correct, Remove Random Name, and all Auto Answer scripts will be broken on the modes. The only one that can kinda be fixed is Auto Answer. In Intervals, there is a set of learnAutoAnswer scripts that learn the answers before solving them. Use them in all modes except Fishing Frenzy and Crypto Hack.")
