333a9a7807cee12fd59ea13e6b25047b0ce31b41
redhorse26 <alexsun@duck.com>
2025-12-10 20:28:48 +0800

    Update maxItems.js

--- cheats/cafe/maxItems.js
+++ cheats/cafe/maxItems.js
@@ -20,60 +20,76 @@
  * Source of original work: [https://github.com/Blooket-Council/Blooket-Cheats/tree/main](https://github.com/Blooket-Council/Blooket-Cheats/tree/main)
  * Source of this modified work: [https://github.com/redhorse26/Blooket-Cheats/tree/main/cheats](https://github.com/redhorse26/Blooket-Cheats/tree/main/cheats)
 */
-/* THE UPDATE CHECKER IS ADDED DURING COMMIT PREP, THERE MAY BE REDUNDANT CODE, DO NOT TOUCH */
 
 (() => {
+    // 1. Setup UI (iframe for clean alerts)
     let iframe = document.querySelector("iframe");
     if (!iframe) {
         iframe = document.createElement("iframe");
         iframe.style.display = "none";
         document.body.append(iframe);
     }
-    /* By CryptoDude3 */
-    if (window.fetch.call.toString() == 'function call() { [native code] }') {
-        const call = window.fetch.call;
-        window.fetch.call = function () {
-            if (!arguments[1].includes("s.blooket.com/rc")) return call.apply(this, arguments);
+    const alert = iframe.contentWindow.alert.bind(window);
+
+    // 2. Main Cheat Logic
+    const cheat = () => {
+        const root = document.querySelector('#app') || document.body;
+        const rKey = Object.keys(root).find(k => k.startsWith('__reactFiber'));
+        
+        if (!rKey) return alert("❌ React Root not found. Please reload.");
+
+        let success = false;
+
+        function traverse(node) {
+            if (success || !node) return;
+
+            if (node.memoizedState) {
+                let hook = node.memoizedState;
+                let index = 0;
+                let levelHook = null;
+
+                // Traverse Hooks to find Index #4
+                while (hook) {
+                    if (index === 4) {
+                        levelHook = hook;
+                        break; 
+                    }
+                    hook = hook.next;
+                    index++;
+                }
+
+                // VALIDATE: Is this the Level Map?
+                // Structure: { Toast: 5, Cereal: 1... }
+                if (levelHook) {
+                    const val = levelHook.memoizedState;
+                    if (val && typeof val === 'object' && !Array.isArray(val) && val.Toast !== undefined) {
+                        
+                        // CREATE HACKED STATE
+                        const newLevels = { ...val };
+                        Object.keys(newLevels).forEach(key => {
+                            if (typeof newLevels[key] === 'number') {
+                                newLevels[key] = 5; // Max Level
+                            }
+                        });
+
+                        // DISPATCH
+                        if (levelHook.queue && levelHook.queue.dispatch) {
+                            levelHook.queue.dispatch(newLevels);
+                            success = true;
+                            alert("✅ All items upgraded to Level 5!");
+                        }
+                    }
+                }
+            }
+            traverse(node.child);
+            traverse(node.sibling);
         }
-    }
-    const timeProcessed = 1730769903219;
-    let latestProcess = -1;
-    const cheat = (async () => {
-        let i = document.createElement('iframe');
-        document.body.append(i);
-        window.alert = i.contentWindow.alert.bind(window);
-        i.remove();
-        if (window.location.pathname !== "/cafe/shop") alert("This can only be run in the shop");
-        else {
-            const { stateNode } = Object.values((function react(r = document.querySelector("body>div")) { return Object.values(r)[1]?.children?.[0]?._owner.stateNode ? r : react(r.querySelector(":scope>div")) })())[1].children[0]._owner;
-            stateNode.setState({ items: Object.keys(stateNode.state.items).reduce((obj, item) => (obj[item] = 5, obj), {}) });
-        }
-    });
-    let img = new Image;
-    img.src = "https://raw.githubusercontent.com/Blooket-Council/Blooket-Cheats/main/autoupdate/timestamps/cafe/maxItems.png?" + Date.now();
-    img.crossOrigin = "Anonymous";
-    img.onload = function() {
-        const c = document.createElement("canvas");
-        const ctx = c.getContext("2d");
-        ctx.drawImage(img, 0, 0, this.width, this.height);
-        let { data } = ctx.getImageData(0, 0, this.width, this.height), decode = "", last;
-        let i = 0;
-        while (i < data.length) {
-            let char = String.fromCharCode(data[i % 4 == 3 ? (i++, i++) : i++] + data[i % 4 == 3 ? (i++, i++) : i++] * 256);
-            decode += char;
-            if (char == "/" && last == "*") break;
-            last = char;
-        }
-        let _, time = timeProcessed, error = "There was an error checking for script updates. Run cheat anyway?";
-        try {
-            [_, time, error] = decode.match(/LastUpdated: (.+?); ErrorMessage: "((.|\n)+?)"/);
-        } catch (e) {}
-        if ((latestProcess = parseInt(time)) <= timeProcessed || iframe.contentWindow.confirm(error)) cheat();
-    }
-    img.onerror = img.onabort = () => {
-        img.onerror = img.onabort = null;
-        cheat();
-        let iframe = document.querySelector("iframe");
-        iframe.contentWindow.alert("It seems the GitHub is either blocked or down.\n\nIf it's NOT blocked, join the Discord server for updates\nhttps://discord.gg/jHjGrrdXP6\n(The cheat will still run after this alert)")
-    }
+
+        traverse(root[rKey]);
+        
+        if (!success) alert("❌ Cheat failed. Make sure you are in the game.");
+    };
+
+    cheat();
+
 })();
