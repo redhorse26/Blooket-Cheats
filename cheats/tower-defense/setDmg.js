f0757efe5b5e819187047c8304ddf459c2ace9f1
redhorse26 <alexsun@duck.com>
2025-12-30 22:48:59 +0800

    Update setDmg.js

--- cheats/tower-defense/setDmg.js
+++ cheats/tower-defense/setDmg.js
@@ -17,56 +17,28 @@
  * 
  * Source: [https://github.com/redhorse26/Blooket-Cheats/tree/main/cheats](https://github.com/redhorse26/Blooket-Cheats/tree/main/cheats)
 */
-/* THE UPDATE CHECKER IS ADDED DURING COMMIT PREP, THERE MAY BE REDUNDANT CODE, DO NOT TOUCH */
-
 (() => {
-    let iframe = document.querySelector("iframe");
-    if (!iframe) {
-        iframe = document.createElement("iframe");
-        iframe.style.display = "none";
-        document.body.append(iframe);
-    }
-    /* By CryptoDude3 */
-    if (window.fetch.call.toString() == 'function call() { [native code] }') {
-        const call = window.fetch.call;
-        window.fetch.call = function () {
-            if (!arguments[1].includes("s.blooket.com/rc")) return call.apply(this, arguments);
+    function getComp(element = document.body, depth = 0) {
+        if (depth > 20) return null;
+        const fiber = element[Object.keys(element).find(k => k.includes('react'))];
+        if (fiber) {
+            let curr = fiber;
+            while (curr) {
+                const sn = curr.stateNode || curr._owner?.stateNode;
+                if (sn?.state?.totalDamage !== undefined) return sn;
+                curr = curr.return;
+            }
+        }
+        for (const child of element.children) {
+            const result = getComp(child, depth + 1);
+            if (result) return result;
         }
     }
-    const timeProcessed = 1730769912574;
-    let latestProcess = -1;
-    const cheat = (async () => {
-        let i = document.createElement('iframe');
-        document.body.append(i);
-        window.prompt = i.contentWindow.prompt.bind(window);
-        i.remove();
-        Object.values((function react(r = document.querySelector("body>div")) { return Object.values(r)[1]?.children?.[0]?._owner.stateNode ? r : react(r.querySelector(":scope>div")) })())[1].children[0]._owner.stateNode.dmg = parseInt(prompt("How much dmg would you like?")) || 0;
-    });
-    let img = new Image;
-    img.src = "https://raw.githubusercontent.com/Blooket-Council/Blooket-Cheats/main/autoupdate/timestamps/tower-defense/setDmg.png?" + Date.now();
-    img.crossOrigin = "Anonymous";
-    img.onload = function() {
-        const c = document.createElement("canvas");
-        const ctx = c.getContext("2d");
-        ctx.drawImage(img, 0, 0, this.width, this.height);
-        let { data } = ctx.getImageData(0, 0, this.width, this.height), decode = "", last;
-        let i = 0;
-        while (i < data.length) {
-            let char = String.fromCharCode(data[i % 4 == 3 ? (i++, i++) : i++] + data[i % 4 == 3 ? (i++, i++) : i++] * 256);
-            decode += char;
-            if (char == "/" && last == "*") break;
-            last = char;
-        }
-        let _, time = timeProcessed, error = "There was an error checking for script updates. Run cheat anyway?";
-        try {
-            [_, time, error] = decode.match(/LastUpdated: (.+?); ErrorMessage: "((.|\n)+?)"/);
-        } catch (e) {}
-        if ((latestProcess = parseInt(time)) <= timeProcessed || iframe.contentWindow.confirm(error)) cheat();
-    }
-    img.onerror = img.onabort = () => {
-        img.onerror = img.onabort = null;
-        cheat();
-        let iframe = document.querySelector("iframe");
-        iframe.contentWindow.alert("It seems the GitHub is either blocked or down.\n\nIf it's NOT blocked, join the Discord server for updates\nhttps://discord.gg/jHjGrrdXP6\n(The cheat will still run after this alert)")
-    }
+    const sn = getComp();
+    if (!sn) return;
+    let iframe = document.createElement('iframe');
+    document.body.append(iframe);
+    const input = iframe.contentWindow.prompt("Enter new Total Damage:", sn.state.totalDamage);
+    iframe.remove();
+    if (input) sn.setState({ totalDamage: parseInt(input) });
 })();
